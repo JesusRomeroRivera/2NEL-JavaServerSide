34504667a7c4ccca563ef3e97dc0c13ac03f7342
Emanuel Josias Ticona Calsín <josias876@hotmail.com>
2021-05-07 17:02:06 -0500

    Actualizando models

--- src/main/java/com/minka/tunel/domain/model/Profile.java
+++ src/main/java/com/minka/tunel/domain/model/Profile.java
@@ -1,8 +1,64 @@
 package com.minka.tunel.domain.model;
 
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
+@Entity
+@Table(name = "profiles")
 public class Profile extends AuditModel {
+    @NotNull
+    @Size(max = 100)
+    @NaturalId
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            mappedBy = "tags")
+    private List<Profile> profiles;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            mappedBy = "tags")
+    private List<Startup> startups;
+
+    public Tag() {
+    }
+
+    public Tag(@NotNull @Size(max = 100) String name) {
+        this.name = name;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Tag setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Tag setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+
+    public List<Profile> getProfiles() {
+        return profiles;
+    }
+
+    public List<Startup> getStartups() {
+        return startups;
+    }
+
+}
     private User User;
     private EMembershipType MembershipType;
     private String FirstName;
@@ -10,4 +66,3 @@ public class Profile extends AuditModel {
     private String Portfolio;
     private List<Profile> ProfileTags;
     private List<Request> Requests;
-}
