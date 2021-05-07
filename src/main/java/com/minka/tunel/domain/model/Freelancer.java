dad88d0acff18fd180d71c24610194d9252c0bc9
Emanuel Josias Ticona Calsín <josias876@hotmail.com>
2021-05-07 17:51:27 -0500

    Actualizando models pt2

--- src/main/java/com/minka/tunel/domain/model/Freelancer.java
+++ src/main/java/com/minka/tunel/domain/model/Freelancer.java
@@ -1,5 +1,38 @@
 package com.minka.tunel.domain.model;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@DiscriminatorValue("freelancer")
 public class Freelancer extends Profile {
-    public String Specialty;
+
+
+
+    public Freelancer(@NotNull @Size(max = 20) String firstName, @NotNull @Size(max = 20) String lastName, @NotNull @Size(max = 100) String portfolio, @NotNull String specialty) {
+        super(firstName, lastName, portfolio);
+        Specialty = specialty;
+    }
+
+    @NotNull
+    private String Specialty;
+
+    public Freelancer(){
+
+    }
+
+    public Freelancer(@NotNull String specialty) {
+        Specialty = specialty;
+    }
+
+    public String getSpecialty() {
+        return Specialty;
+    }
+
+    public Freelancer setSpecialty(String specialty) {
+        Specialty = specialty;
+        return this;
+    }
 }
--- src/main/java/com/minka/tunel/domain/model/Investor.java
+++ src/main/java/com/minka/tunel/domain/model/Investor.java
@@ -1,5 +1,10 @@
 package com.minka.tunel.domain.model;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("investor")
 public class Investor extends Profile {
 
 }
